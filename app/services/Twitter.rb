640386de6eabfd588094799e95b83944fc1bcbf8
Elmousski <a.moussier@hotmail.fr>
2018-11-15 18:12:03 +0100

    service

--- app/services/Twitter.rb
+++ app/services/Twitter.rb
@@ -8,23 +8,20 @@ client = Twitter::REST::Client.new do |config|
    config.consumer_secret     = ENV["CONSUMER_SECRET"]
    config.access_token        = ENV["ACCESS_TOKEN"]
    config.access_token_secret = ENV["ACCESS_SECRET"]
- 
-   end
+ end
 
-tab_tags = []
-tab_tags << "developpeur"
-tab_user = []while true
+    tab_tags = []
+    tab_tags << "developpeur"
+    tab_user = []while true
 
 
-       tab_tags.each do |tag|
-       client.search(tag).take(10).collect do |tweet|
-             #puts "#{Time.now.utc} - #{tweet.created_at} -  #{tweet.user.screen_name}: #{tweet.text}"
-       tab_user << tweet.user.screen_name
-           #puts tab_user
-       tab_user.each do |user|
-       client.update("Salut, @#{user} viens faire un tour ici : http...")
-       
-         end
-       end
-end
+tab_tags.each do |tag|
+  client.search(tag).take(10).collect do |tweet|
+         #puts "#{Time.now.utc} - #{tweet.created_at} -  #{tweet.user.screen_name}: #{tweet.text}"
+    tab_user << tweet.user.screen_name
+       #puts tab_user
+    tab_user.each do |user|
+      client.update("Salut, @#{user} viens faire un tour ici : http...")
+    end
+  end
 end
